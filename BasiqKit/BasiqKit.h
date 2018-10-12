9d38695dc9cdeab3e15330f00ab53560936fb93b
Eugene Velizhenkov <tredasaibot@gmail.com>
2018-10-12 14:20:34 +0200

    Initial Commit

---
+++ BasiqKit/BasiqKit.h
@@ -0,0 +1,19 @@
+//
+//  BasiqKit.h
+//  BasiqKit
+//
+//  Created by Yevhen Velizhenkov on 10/12/18.
+//  Copyright © 2018 tapkain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasiqKit.
+FOUNDATION_EXPORT double BasiqKitVersionNumber;
+
+//! Project version string for BasiqKit.
+FOUNDATION_EXPORT const unsigned char BasiqKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasiqKit/PublicHeader.h>
+
+
